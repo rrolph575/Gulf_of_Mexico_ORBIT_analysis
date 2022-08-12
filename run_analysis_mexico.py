44b8eac91c6bfef428a3240c47d3e9c1f6b378e8
Rolph <rrolph@nrel.gov>
2022-08-12 16:42:01 -0600

    added yamls and initial run scripts

--- run_analysis_mexico.py
+++ run_analysis_mexico.py
@@ -10,27 +10,19 @@ import os
 from ORBIT import ProjectManager, load_config
 from ORBIT.core.library import initialize_library
 
-# Import specialized Ocergy design and installation modules
-from design_semi_Ocergy import CustomOCGSemiSubDesign
-from mooring_system_design_OCG import CustomOCGMooringSystemDesign
-from quayside_assembly_tow_moored_OCG import CustomOCGMooredSubInstallation
-from mooring_OCG import CustomMooringSystemInstallation
-
 # Import soft cost analysis module
 from construction_finance_param import con_fin_params
 
-
-#initialize_library("data")
 DIR = os.path.split(__file__)[0]
 LIBRARY= os.path.join(DIR, "library")
 print(LIBRARY)
 initialize_library(LIBRARY)
 
-WEATHER = pd.read_csv('library/weather/Mayflower.csv')
+WEATHER = pd.read_csv('library/weather/example_weather.csv')
 
 loop_through_configs = False # if True, will run ORBIT for all config files saved in configs/.  If false, specify config file you wish to run:
 if loop_through_configs == False:
-    specific_config_filename_to_run = 'configs/semisub_OCG.yaml'
+    specific_config_filename_to_run = 'configs/feeder_jacket_install.yaml'
 
 def run():
 
@@ -39,14 +31,6 @@ def run():
     breakdowns = {}
     installation_times = {}
     bos_capex_per_kW = {}
-
-    ProjectManager._design_phases.append(CustomOCGSemiSubDesign)
-    ProjectManager._design_phases.append(CustomOCGMooringSystemDesign)
-    #print(ProjectManager._design_phases)
-    
-    ProjectManager._install_phases.append(CustomOCGMooredSubInstallation)
-    ProjectManager._install_phases.append(CustomMooringSystemInstallation)
-    
 
     if loop_through_configs == True:
         print('this script is for only one config, if more then copy the code for the single config analysis')
@@ -59,12 +43,8 @@ def run():
             # Collect Results
             bos_capex[name] = project.bos_capex 
             total_capex[name] = project.total_capex
-            
-            #breakdowns[name] = project.capex_breakdown
-            
-            #"""
-            ## Commented here because edited with soft cost analysis module immediately below.
-            
+
+            # Implement update how soft costs are calculated.
             dct = project.capex_breakdown
             dct = {k:[v] for k,v in dct.items()}
             df_bos_breakdown = pd.DataFrame.from_dict(dct, orient="columns")
@@ -80,7 +60,7 @@ def run():
                     df_bos_breakdown["Soft"] = v
             df_bos_breakdown = df_bos_breakdown.T
             print(df_bos_breakdown)
-            breakdowns = df_bos_breakdown
+            breakdowns = df_bos_breakdown.drop("Soft")  ## Drop soft costs so they are not calculated twice in the sum below. Because the soft cost components are also part of this dataframe.
             #breakdowns[name] = dct
             #"""
             installation_times[name] = project.project_time
@@ -88,17 +68,9 @@ def run():
 
     # Aggregate Results
     bos_capex = pd.Series(bos_capex)
-    #total_capex = pd.Series(total_capex)
     total_capex = df_bos_breakdown.sum()
     
-    """
-    breakdowns = pd.DataFrame(breakdowns)
-    data_for_breakdowns = np.empty(breakdowns.values.shape[0])
-    for i in np.arange(0, breakdowns.values.shape[0]):
-        data_for_breakdowns[i] = breakdowns.values[i][0][0]
 
-    breakdowns = pd.DataFrame(data={name: data_for_breakdowns}, index = breakdowns.index)
-    """
     times = pd.Series(installation_times)
     bos_capex_per_kW = pd.Series(bos_capex_per_kW)
 
@@ -118,7 +90,6 @@ def run():
     
     return project, total_capex, breakdowns, df_bos_breakdown, totals, soft_dct
     
-## can add in an append_capex def here if e.g. we need to add transport costs (hardcoded)
 
 def test_capex_breakdown(name, project):
 
@@ -159,17 +130,8 @@ Index(['cost_multiplier', 'agent', 'action', 'duration', 'cost', 'level',
        'transit_speed', 'location', 'site_depth', 'num_vessels'],
       dtype='object')
 """
-
      
-print('anchor mass is: ' + str(project.detailed_outputs['anchor_mass']))
-print('line mass is: ' + str(project.detailed_outputs['line_mass']))
-print('anchor type is: ' + str(project.detailed_outputs['anchor_type']))
-
-# vessel efficiency:
-#print('vessel efficiency for mooring installation: ' + #str(project.detailed_outputs['CustomMooringSystemInstallation_OCG']))
-
-# number of times per action is called
-df.groupby("action").count()['time']
+#print('anchor mass is: ' + str(project.detailed_outputs['anchor_mass']))
     
     
 
