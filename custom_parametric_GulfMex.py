5ed79bd8b5b5785d88b45bd816adb3eb45e1ad3d
Becca Rolph <rebecca.rolph@nrel.gov>
2022-11-11 14:34:20 -0700

    adjusted jacket install equations for 600MW farm, 40 turbines for 15MW and 35 turbines for 17MW

--- custom_parametric_GulfMex.py
+++ custom_parametric_GulfMex.py
@@ -12,47 +12,41 @@ from itertools import product
 from copy import deepcopy
 import os
 from scipy.optimize import curve_fit
-#from mpl_toolkits.mplot3d import axes3d, Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 from ORBIT import ProjectManager, load_config
-from ORBIT.parametric import LinearModel
 from ORBIT.core.library import initialize_library
 
-# define paths
+### Define paths
 fig_path = 'C:/Users/rrolph/OneDrive - NREL/Projects/Gulf_of_Mexico/plots_from_modelling_ORBIT_output/'
-
-depths = [10, 20, 30, 40, 50, 60]  # m. water depth
-turbines = ["15mw", "17mw", "17mw-lowSP"]
-distances = [10, 50, 100, 150, 200, 300, 400, 500, 600] # km. distance from site to port
-
 DIR = os.path.split(__file__)[0]
 LIBRARY= os.path.join(DIR, "library")
-print(LIBRARY)
 initialize_library(LIBRARY)
 
-# Create or load a base config of constant inputs
+
+### Define independent variables
+depths = [10, 20, 30, 40, 50, 60]  # water depth [m]
+turbines = ["15mw", "17mw", "17mw-lowSP"]
+distances = [10, 50, 100, 150, 200, 300, 400, 500, 600] # distance from site to port [km]
+
+
+### Create or load a base config of constant inputs
 base_config = {
     
     # Constant Inputs
     "wtiv": "example_wtiv",
-    #feeder: "dict | str (optional)",
-    #num_feeders: "int (optional)",
-    "plant": {"num_turbines": 1},
-    #port:
-        #num_cranes: #"int (optional, default: 1)",
-        #monthly_rate: #"USD/mo (optional)",
-        #name: # "str (optional)",
     
     # Constant Modules
-    "design_phases": [], # We are providing the design from Mayank's jacket yaml file, not an ORBIT design module.
+    "design_phases": [], # We are providing the design from a jacket yaml file, not an ORBIT design module.
     "install_phases": [
         "JacketInstallation"
     ]
 }
 
+
+### Loop through independent variables and run ORBIT to get costs using each arrangement of indep. variables.
 # Create a list to hold results
 data = []
 
@@ -62,9 +56,14 @@ for depth, turbine, dts in product(depths, turbines, distances):
     # Copy base config into new object so that you don't impact the base config with any dict operations
     config = deepcopy(base_config)    
 
+    if turbine == '15mw':
+        num_turbines = 40
+    if turbine != '15mw':
+        num_turbines = 35
     # Load scenario specific info that changes with loop
     dict_scenario = {
-        'site': {'depth': depth, 'distance': dts},        
+        'site': {'depth': depth, 'distance': dts},
+        'plant': {'num_turbines': num_turbines},        
         }
     
     # combine configs
@@ -75,23 +74,18 @@ for depth, turbine, dts in product(depths, turbines, distances):
     turbine_config = load_config(f"{sub_config_fp}/turbines/{turbine}/{turbine}.yaml")
 
     # Merge new base and scenario specific configs
-    #config = ProjectManager.merge_dicts(sub_config, base_config)
     config = ProjectManager.merge_dicts(config, jacket_config)
     config = ProjectManager.merge_dicts(config,turbine_config)
 
     # Run ORBIT, pass weather if necessary
     project = ProjectManager(config, library_path=LIBRARY)
     project.run()
-    
-    
+        
     ## debug distance vs. transit time
-    if (depth == 60 and dts == 600 and turbine == '15mw') or (depth == 10 and dts == 600 and turbine == '15mw') or (depth == 10 and dts == 200 and turbine == '15mw'):
-        df_actions = pd.DataFrame(project.actions)
-        sub_actions = df_actions.loc[df_actions['phase']=='JacketInstallation']
-        sub_actions.action
-        print('site distance is: ' + str(project.config["site.distance"]) + '. Depth is ' + str(project.config["site.depth"]))
-        print(sub_actions[['action','duration', 'cost']])
-        ##
+    #if (depth == 60 and dts == 600 and turbine == '15mw') or (depth == 10 and dts == 600 and turbine == '15mw') or (depth == 10 and dts == 200 and turbine == '15mw'):
+    #    df_actions = pd.DataFrame(project.actions)
+    #    sub_actions = df_actions.loc[df_actions['phase']=='JacketInstallation']
+    #    sub_actions.action
 
     # Collect required inputs and results
     res = {
@@ -105,30 +99,16 @@ for depth, turbine, dts in product(depths, turbines, distances):
 
     data.append(res)
 
+
 # Create DataFrame
 df = pd.DataFrame(data)
 
 # Save dataframe
 df.to_pickle('outputs/jacket_installation_cost_1_turbine.pkl')
 
-'''
-sub = df.loc[df.site_depth == 10]
-sub.iloc[10]
-Out[62]: 
-site_depth           1.000000e+01
-distance_to_shore    5.000000e+01
-jacket_install       3.353009e+07
-Name: 10, dtype: float64
 
-sub.iloc[0]
-Out[63]: 
-site_depth           1.000000e+01
-distance_to_shore    1.000000e+01
-jacket_install       3.321548e+07
-Name: 0, dtype: float64
-'''
 
-## Plot some results
+### Plot some results
 
 # create 3D plane equation for model fit
 def func(data, a, b, c):  # make sure data is a list, with first index dts, and 2nd index site depth
@@ -137,6 +117,8 @@ def func(data, a, b, c):  # make sure data is a list, with first index dts, and 
     return a*dts + b*site_depth + c    
     
 def plot_3d(df, turbine_rating, fig_path):  # df is the dataframe that contains information for all or one turbines
+
+    ### plot the results of the runs 
     df = df[df.turbine_rating==turbine_rating]
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(projection = '3d')
@@ -150,17 +132,19 @@ def plot_3d(df, turbine_rating, fig_path):  # df is the dataframe that contains 
     # find model parameters. jacket_install_cost = f(dts, depth)
     params, pcov = curve_fit(func, [df.distance_to_shore.values, df.site_depth.values], df.jacket_install_cost)
 
-    # create surface model
+
+
+    ### create surface model
     # set up data points for calculating surface model
     model_dts_data = np.linspace(min(df.distance_to_shore.values), max(df.distance_to_shore.values),len(df))
     model_depth_data = np.linspace(min(df.site_depth.values), max(df.site_depth.values), len(df))
-
-    # create coordinate arrays
+    
+    # create coordinate arrays of the model data
     DTS, DEPTH = np.meshgrid(model_dts_data, model_depth_data)
     # calculate Z coordinate array
     INSTALL_COST = func([DTS, DEPTH], *params)
 
-    # plot new 3D plot with data and model overlaid
+    # plot 3D plot with data and model overlaid
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(projection = '3d')
     # "data" (output by ORBIT)
@@ -174,39 +158,35 @@ def plot_3d(df, turbine_rating, fig_path):  # df is the dataframe that contains 
     
     return params, pcov
 
+
+
 # Call each turbine rating
 for turbine in turbines:
+    print(turbine)
     if turbine == '15mw':
+        #print('this is run')
         turbine_rating = 15.0
-    if turbine == '17mw' or '17mw-lowSP':
+        num_turbines = 40
+        print(turbine + str(num_turbines))
+        #print(num_turbines)
+    if turbine != '15mw':
         turbine_rating = 17.0
+        num_turbines = 35
+        print(turbine + str(num_turbines))
+    
+    print(turbine + str(num_turbines))
+    
+    ''' why does the above loop give 15mw35?
+    15mw40
+    15mw35
+    17mw35
+    17mw-lowSP35
+    '''
+    
     params, pcov = plot_3d(df, turbine_rating, fig_path)
-    print('Plot generated and saved for turbine: ' + turbine)
-    print('Model equation for ' + turbine + ': ' + 'jacket_install_cost = ' + str(params[0]) + '*dts + ' + str(params[1]) + '*site_depth + ' + str(params[2]))
-
-'''
-    Output of above code:
-    Plot generated and saved for turbine: 15mw
-    Model equation for 15mw: jacket_install_cost = 1023.9726027397224*dts + 136.52968036531928*site_depth + 2502420.091324202
-    Plot generated and saved for turbine: 17mw
-    Model equation for 17mw: jacket_install_cost = 1023.9726027397224*dts + 136.52968036531928*site_depth + 2502420.091324202
-    Plot generated and saved for turbine: 17mw-lowSP
-    Model equation for 17mw-lowSP: jacket_install_cost = 1023.9726027397224*dts + 136.52968036531928*site_depth + 2502420.091324202
-'''
-# Test model with data at a random point
-# 15mw 
-dts = 150 # km
-site_depth = 50
-turbine = '15mw'
-if turbine == '15mw':
-    jacket_install_cost_modelled = 1023.9726027397224*dts + 136.52968036531928*site_depth + 2502420.091324202
-print('jacket_install_cost_modelled for dts ' + str(dts) + ' and depth ' + str(site_depth) + ': ' + str(jacket_install_cost_modelled))
-#jacket_install_cost_ORBIT = 
-# Note the below will only work if dts and site_depth exist in ORBIT inputs
-if turbine == '15mw':
-    jacket_install_cost_orbit = df.loc[(df['distance_to_shore']==dts) & (df['site_depth']==site_depth) & (df['turbine_name']==turbine)].jacket_install_cost
-    print('jacket install cost from ORBIT: ' + str(jacket_install_cost_orbit.values))
-
+    params_adj_to_num_turbines = params/num_turbines
+    print('Plot generated and saved for turbine: ' + turbine + ' divided by ' + str(num_turbines) + ' number of turbines.')
+    print('Model equation for ' + turbine + ': ' + 'jacket_install_cost = ' + str(params_adj_to_num_turbines[0]) + '*dts + ' + str(params_adj_to_num_turbines[1]) + '*site_depth + ' + str(params_adj_to_num_turbines[2]))
 
 
 '''
